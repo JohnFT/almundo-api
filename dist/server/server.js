7e864dd38e8d87c3f8dc52e7e9233e28a5935eea
JohnF <johnf@digitalware.com.co>
2019-03-26 17:22:46 -0500

    'cors'

--- dist/server/server.js
+++ dist/server/server.js
@@ -6,14 +6,19 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var routes_1 = require("../routes");
 var swaggerUi = __importStar(require("swagger-ui-express"));
+var cors_1 = __importDefault(require("cors"));
 var Server = (function () {
     function Server(port) {
         this.port = port;
         this.app = express();
+        this.app.use(cors_1.default());
         routes_1.RegisterRoutes(this.app);
         try {
             var swaggerDocumnet = require("../../swagger.json");
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express = require("express");
 import { RegisterRoutes } from "../routes";
 import * as swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 // create server express
 export default class Server {
@@ -11,13 +12,20 @@ export default class Server {
   constructor(private port: number) {
     this.app = express();
 
+    // coors
+    this.app.use(cors());
+
     // Registe urls app
     RegisterRoutes(this.app);
 
     // intance swagger
     try {
       const swaggerDocumnet = require("../../swagger.json");
-      this.app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumnet));
+      this.app.use(
+        "/api/v1/docs",
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerDocumnet)
+      );
     } catch (err) {
       console.log(err);
     }

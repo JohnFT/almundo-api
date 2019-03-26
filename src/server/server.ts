59c4f5dff60e3a9a2f55bf9fb2824ebec34d95f9
JohnF <johnf@digitalware.com.co>
2019-03-26 11:38:23 -0500

    pacakge

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,6 @@ import express = require("express");
 import { RegisterRoutes } from "../routes";
 import * as swaggerUi from "swagger-ui-express";
 
-
 // create server express
 export default class Server {
   // propierties
@@ -31,6 +30,6 @@ export default class Server {
 
   // start server on port
   start(callback: Function) {
-    this.app.listen(this.port, callback());
+    this.app.listen(process.env.PORT || this.port, callback());
   }
 }
